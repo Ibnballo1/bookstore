05f74b9f0bf4664d92887bf1c43f9f47bd682106
Ibnballo1 <belloabdullateef035@gmail.com>
2022-09-16 00:30:45 +0100

    Add Books for list of books

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
+import Books from './components/Books';
+import './App.css';
 
 const App = () => (
   <div>
     <Header />
+    <Books />
   </div>
 );
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => (
-  <header>
+  <header className="headers">
     <div>
       <h1>Bookstore CMS</h1>
       <nav>
