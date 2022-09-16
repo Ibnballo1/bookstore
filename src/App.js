3f056b95bc7eece01dfa94e3fc819acd88e44c44
Ibnballo1 <belloabdullateef035@gmail.com>
2022-09-16 01:24:30 +0100

    Add category component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Books from './components/Books';
+import Categories from './components/Categories';
 import './App.css';
 
 const App = () => (
-  <div>
+  <div className="container">
     <Header />
-    <Books />
+    <Routes>
+      <Route path="/" element={<Books />} />
+      <Route path="/categories" element={<Categories />} />
+    </Routes>
   </div>
 );
 
