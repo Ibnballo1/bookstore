8dc599af265db0ae119c3a430895e6511def1312
Ibnballo1 <belloabdullateef035@gmail.com>
2022-09-20 15:55:42 +0100

    Add books to booklist

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,13 +1,23 @@
+/* eslint-disable react/no-array-index-key */
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const Book = () => (
-  <section>
-    <div>
-      <h1>Book Title</h1>
-      <p>Book Author</p>
-    </div>
-    <button type="button">Remove</button>
-  </section>
-);
+const Book = () => {
+  const books = useSelector((state) => state.bookReducer);
+  console.log(books);
+  const book = books.map((book, index) => (
+    <h1 key={index}>{book}</h1>
+  ));
+
+  return (
+    <section>
+      <div>
+        {book}
+        <p>Book Author</p>
+      </div>
+      <button type="button">Remove</button>
+    </section>
+  );
+};
 
 export default Book;
