4b349035aedecec390e2e41e69b70613a8917f16
Ibnballo1 <belloabdullateef035@gmail.com>
2022-09-23 18:48:03 +0100

    Add styles to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => (
   <header className="headers">
     <div className="logo-nav">
       <h1 className="logo">Bookstore CMS</h1>
       <nav className="nav">
-        <Link to="/">Books</Link>
-        <Link to="categories">Categories</Link>
+        <Link className="nav-book" to="/">Books</Link>
+        <Link className="nav-category" to="categories">Categories</Link>
       </nav>
     </div>
-    <div>
-      <i>Profile Pics</i>
+    <div className="profile-container">
+      <FontAwesomeIcon className="user" icon={faUser} />
     </div>
   </header>
 );
